98c5dd14c82856c25715f03faba7733a2b6e35ad
Duncan Brown <duncanjbrown@gmail.com>
2017-02-05 21:23:30 +0000

    Update spacemacs

---
+++ emacs.d/elpa/better-defaults-0.1.3/better-defaults-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "better-defaults" "0.1.3" "Fixing weird quirks and poor defaults" 'nil :url "https://github.com/technomancy/better-defaults" :keywords '("convenience"))
