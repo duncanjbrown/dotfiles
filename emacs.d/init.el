c240f3ab324c85708ea343024bddc67a48ba438c
Duncan Brown <duncanjbrown@gmail.com>
2017-02-19 19:38:18 +0000

    Make flagged.org in same folder as other orgs
    This appears to make a difference to how flagged things are sucked into
    new files.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -70,7 +70,7 @@
 ;; Set to the location of your Org files on your local system
 (setq org-directory "~/Dropbox/org")
 ;; Set to the name of the file where new notes will be stored
-(setq org-mobile-inbox-for-pull "~/Dropbox/flagged.org")
+(setq org-mobile-inbox-for-pull "~/Dropbox/org/flagged.org")
 ;; Set to <your Dropbox root directory>/MobileOrg.
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 ;; Location of all files to be synced to MobileOrg https://groups.google.com/forum/#!topic/mobileorg-android/TcoseKmXShc
