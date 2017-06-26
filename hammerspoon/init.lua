cfffc0a0e2aa4752ef92ef6352589d93f2844fbb
Duncan Brown <duncan.brown@dxw.com>
2017-06-26 13:55:33 +0100

    Add maximize to HS

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -16,6 +16,19 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
   win:setFrame(f)
 end)
 
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "\\", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
