b710e58463311a522aa23b9b84f63cb2483d3653
Duncan Brown <duncanjbrown@gmail.com>
2023-11-30 20:11:29 +0000

    Actually configure conjure

--- config/nvim/lua/conjure.lua
+++ config/nvim/lua/conjure.lua
@@ -1,6 +1,6 @@
 -- conjure
-vim.g.conjure_log_wrap = 'true'
-vim.g.conjure_extract_tree_sitter_enabled = 'true'
 vim.g.clojure_align_subforms = 1
-vim.g.conjure_filetypes = {'clojure'}
+vim.g["conjure#extract_tree_sitter_enabled"] = 'true'
+vim.g["conjure#log_wrap"] = 'true'
+vim.g["conjure#filetypes"] = { 'clojure' }
 
