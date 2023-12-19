5cb95d55335ed2932606f967685fd6d5f949b9c5
Duncan Brown <duncanjbrown@gmail.com>
2023-12-19 16:08:18 +0000

    Remove marksman LSP
    This requires a local npm package, which isn't convenient

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -1,6 +1,7 @@
 local nvim_lsp = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-nvim_lsp.solargraph.setup{
+
+nvim_lsp.solargraph.setup {
   cmd = { "bundle", "exec", "solargraph", "stdio" },
   capabilities = capabilities,
   settings = {
@@ -10,19 +11,20 @@ nvim_lsp.solargraph.setup{
   }
 }
 
-nvim_lsp.html.setup{
+nvim_lsp.html.setup {
   capabilities = capabilities,
-  filetypes = {'html', 'htmldjango'}
+  filetypes = { 'html', 'htmldjango' }
 }
 
-nvim_lsp.cssls.setup{
+nvim_lsp.cssls.setup {
   capabilities = capabilities,
-  filetypes = {'css', 'scss'}
+  filetypes = { 'css', 'scss' }
 }
 
 nvim_lsp.clojure_lsp.setup {
   capabilities = capabilities,
 }
+
 nvim_lsp.pylsp.setup {
   capabilities = capabilities,
   settings = {
@@ -34,6 +36,7 @@ nvim_lsp.pylsp.setup {
     }
   }
 }
+
 nvim_lsp.lua_ls.setup {
   capabilities = capabilities,
   settings = {
@@ -46,17 +49,15 @@ nvim_lsp.lua_ls.setup {
     }
   }
 }
-nvim_lsp.marksman.setup{
+
+nvim_lsp.marksman.setup {
   capabilities = capabilities,
 }
-nvim_lsp.remark_ls.setup {
-  settings = {
-    requireConfig = true
-  }
-}
+
 nvim_lsp.tsserver.setup {
   capabilities = capabilities,
 }
+
 nvim_lsp.rust_analyzer.setup {
   capabilities = capabilities,
   settings = {
@@ -85,7 +86,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>nn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set({'n', 'v'}, '<space>f', function()
+    vim.keymap.set({ 'n', 'v' }, '<space>f', function()
       vim.lsp.buf.format { async = true }
     end, opts)
   end,
