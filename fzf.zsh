ebeb8098f68e60ef6d473c57cc3a38d403927cb7
Duncan Brown <duncanjbrown@gmail.com>
2024-04-09 13:01:31 +0100

    upgrade fzf

--- fzf.zsh
+++ fzf.zsh
@@ -4,10 +4,4 @@ if [[ ! "$PATH" == */Users/duncan.brown/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/Users/duncan.brown/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-source "/Users/duncan.brown/.fzf/shell/completion.zsh"
-
-# Key bindings
-# ------------
-source "/Users/duncan.brown/.fzf/shell/key-bindings.zsh"
+eval "$(fzf --zsh)"
